68fff752c0ab71dd3cf62b982e239516581a66f1
trulsmp <Truls@dhcp-038051.wlan.ntnu.no>
2014-03-06 14:20:29 +0100

    added java folder and basic project setup

---
+++ java/src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by Truls on 06.03.14.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("First String from Program, Aweesommmme");
+    }
+
+
+}
