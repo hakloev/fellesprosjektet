49af1e925d6c3576bbe1f10a785276cd972bcf91
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-03-11 10:43:10 +0100

    started request

--- server/src/helperclasses/Request.java
+++ server/src/helperclasses/Request.java
@@ -1,11 +1,23 @@
 package helperclasses;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Håkon Ødegård Løvdal on 10/03/14.
  */
 public class Request {
 
-	public Request() {
+	private final String _TIMEOFREQUEST;
 
+	public Request() {
+		_TIMEOFREQUEST = getTime();
+	}
+
+	public static String getTime() {
+		DateFormat df = new SimpleDateFormat("HH:mm:ss");
+		Date date = new Date();
+		return df.format(date);
 	}
 }
