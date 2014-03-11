bd62e1aec268f2e1f310865fa1eafc3b204b4f06
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-03-11 15:58:35 +0100

    work in progress

--- server/src/helperclasses/Request.java
+++ server/src/helperclasses/Request.java
@@ -1,8 +1,16 @@
 package helperclasses;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
+
+import models.*;
+import sun.org.mozilla.javascript.internal.json.JsonParser;
 
 
 /**
@@ -19,6 +27,41 @@ public class Request {
 	}
 
 	public Object parseJSON() { // this is supposed to return any given object, must be casted
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode root = mapper.readTree(_JSONREQUEST);
+			Iterator<JsonNode> iterator = root.iterator();
+			int count = 0;
+			while (iterator.hasNext()) {
+				JsonNode node = iterator.next();
+				System.out.println(node);
+				if (count == 2) {
+					System.out.println("inside");
+					return mapper.readValue(String.valueOf(node), Employee.class);
+				}
+				count++;
+			}
+
+		/*	if (classTypeString.equals("appointment")) {
+				// TODO return createAppointmentObject();
+			} else if (classTypeString.equals("employee")) {
+				return mapper.readValue(root.path("object").traverse(), Employee.class);
+			} else if (classTypeString.equals("groupname")) {
+				// TODO
+			} else if (classTypeString.equals("meetingroom")) {
+				// TODO
+			} else if (classTypeString.equals("warning")) {
+				// TODO
+			} else {
+				// TODO
+			}*/
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+		//User userFromJSON = mapper.readValue(userDataJSON, User.class);
+		//System.out.println(userFromJSON);
 		return null;
 	}
 
