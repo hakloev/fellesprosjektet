c4ff4e2e6f5283f8e246eac5621bf5f34188d9f2
trulsmp <Truls@dhcp-038174.wlan.ntnu.no>
2014-03-13 10:41:03 +0100

    combined main functions

---
+++ client/src/main/Client.java
@@ -0,0 +1,31 @@
+package main;
+import controllers.SocketListener;
+import gui.CalendarView;
+
+import javax.swing.*;
+
+/**
+ * Created by Truls on 13.03.14.
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        //Creating a SocketClient object
+        SocketListener client = new SocketListener ("localhost",4658);
+        //trying to establish connection to the server
+        client.connect();
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        new CalendarView();
+
+
+
+    }
+}
+
+
